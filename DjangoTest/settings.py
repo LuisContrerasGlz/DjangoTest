5ac62b91c7884ac664b333df55f415cf2b0ff5f9
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2023-04-08 15:53:24 -0600

    security

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wiiy*6q3%8v^8(l_x&hep1*_3+9=@57)k2!!rb-&ylpyglke5u'
+# SECURITY WARNING: keep the secret key used in production secret! key is removed for security 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
